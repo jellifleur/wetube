e6d5ac7708b29f531c0b9e91a0e0553a66db7d53
jhchoi08 <jhchoi08@cafe24corp.com>
2020-02-24 20:48:03 +0900

    ~#2.17

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
+import { localMiddleware } from './middlewares';
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+app.use(localMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
