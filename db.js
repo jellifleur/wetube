cd2e5be12896bb644bc261f77f193d81a17be857
jhchoi08 <jhchoi08@cafe24corp.com>
2020-02-27 22:41:20 +0900

    ~#2.25

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:321345,
+        title: "Video awesome",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 14234,
+            name: "Jaehee",
+            email: "jh123@123.com"
+        }
+    },
+    {
+        id:553324,
+        title: "Special Apples",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 14234,
+            name: "Jaehee",
+            email: "jh123@123.com"
+        }
+    },
+    {
+        id:623452,
+        title: "Oh perfect",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 14234,
+            name: "Jaehee",
+            email: "jh123@123.com"
+        }
+    },
+    {
+        id:745624,
+        title: "Hello video",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id: 14234,
+            name: "Jaehee",
+            email: "jh123@123.com"
+        }
+    }
+];
