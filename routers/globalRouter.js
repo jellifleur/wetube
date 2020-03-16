1085f2e6c63c7135992c1f534cda29175e751d2d
jhchoi08 <jhchoi08@cafe24corp.com>
2020-03-16 18:21:39 +0900

    ~#6.5

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -2,14 +2,15 @@ import express from "express";
 import routes from "../routes";
 import { home, search } from '../controller/videoController';
 import { getJoin, postJoin, getLogin, postLogin, logout } from '../controller/userController';
+import { onlyPublic } from '../middlewares';
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.logout, logout);
 
