52a634b396c911a60fdab4145827550e22e16a5c
jhchoi08 <jhchoi08@cafe24corp.com>
2020-03-17 14:41:04 +0900

    ~#6.10

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
+import passport from 'passport';
 import routes from "../routes";
 import { home, search } from '../controller/videoController';
-import { getJoin, postJoin, getLogin, postLogin, logout } from '../controller/userController';
-import { onlyPublic } from '../middlewares';
+import { getJoin, postJoin, getLogin, postLogin, logout, githubLogin, postGithubLogIn, getMe, facebookLogin, postFacebookLogin } from '../controller/userController';
+import { onlyPublic, onlyPrivate } from '../middlewares';
 
 const globalRouter = express.Router();
 
@@ -12,9 +13,25 @@ globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 globalRouter.get(routes.login, onlyPublic, getLogin);
 globalRouter.post(routes.login, onlyPublic, postLogin);
 
-globalRouter.get(routes.logout, logout);
+globalRouter.get(routes.logout, onlyPrivate, logout);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
+
+globalRouter.get(routes.github, githubLogin);
+globalRouter.get(
+    routes.githubCallback,
+    passport.authenticate("github", { failureRedirect: "/login" }),
+    postGithubLogIn
+);
+
+globalRouter.get(routes.facebook, facebookLogin);
+globalRouter.get(
+    routes.facebookCallback,
+    passport.authenticate("facebook", { failureRedirect: "/login" }),
+    postFacebookLogin
+);
+
+globalRouter.get(routes.me, getMe);
 
 export default globalRouter;
--- routes.js
+++ routes.js
@@ -10,6 +10,7 @@ const USERS = "/users";
 const USER_DETAIL = "/:id"
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
+const ME = "/me";
 
 // Video
 const VIDEOS = "/videos";
@@ -17,6 +18,14 @@ const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
+
+// Github
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
+
+// Facebook
+const FB = "/auth/facebook";
+const FB_CALLBACK = "/auth/facebook/callback";
 
 const routes = {
     home: HOME,
@@ -34,6 +43,7 @@ const routes = {
     },
     editProfile: EDIT_PROFILE,
     changePassword: CHANGE_PASSWORD,
+    me: ME,
     videos: VIDEOS,
     upload: UPLOAD,
     videoDetail: (id) => {
@@ -56,7 +66,11 @@ const routes = {
         } else {
             return DELETE_VIDEO;
         }
-    }
+    },
+    github: GITHUB,
+    githubCallback: GITHUB_CALLBACK,
+    facebook: FB,
+    facebookCallback: FB_CALLBACK
 };
 
 export default routes;
