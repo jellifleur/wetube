7cfacd8447c4568e2e205ff4c529995a3a3c1a4c
jellifleur <jhchoi08.cafe24@gmail.com>
2020-03-26 23:10:18 +0900

    flash

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import passport from "passport";
 import mongoose from "mongoose";
 import session from "express-session";
 import path from "path";
+import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import { localMiddleware } from './middlewares';
 import routes from "./routes";
@@ -36,6 +37,8 @@ app.use(session({
     saveUninitialized: false,
     store: new CookieStore({ mongooseConnection: mongoose.connection })
 }));
+app.use(flash());
+
 app.use(passport.initialize());
 app.use(passport.session());
 
