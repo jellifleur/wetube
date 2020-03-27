06452af57f6ca9d55c72c6d5e5c35c3ca74ab997
jellifleur <jhchoi08.cafe24@gmail.com>
2020-03-28 02:37:48 +0900

    style

--- src/assets/js/videoSize.js
+++ src/assets/js/videoSize.js
@@ -1,21 +1,15 @@
-
-
-
 window.addEventListener("load", () => {
     const videoPlayer = document.getElementById("jsVideoPlayer");
     const video = document.getElementById("jsVideo");
 
     const setVideoHeight = () => {
-        console.log(video.clientHeight);
         if (video.clientHeight > 500) {
             videoPlayer.classList.add("verticalVideo");
         }
     }
 
-    console.log("ssssbbb" + videoPlayer.clientHeight);
     if (video) {
         setVideoHeight();
         //video.addEventListener("loadeddata", setVideoHeight);
-        //document.addEventListener("DOMContentLoaded", setVideoHeight);
     }
 });
